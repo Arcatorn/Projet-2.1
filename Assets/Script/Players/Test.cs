00dab63c001006b645c9097738b80a5bf601e25a
Warlooks <44344597+Warlooks@users.noreply.github.com>
2018-11-30 23:21:07 +0100

    Shader

---
+++ Assets/Script/Players/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	Material m;
+
+
+
+	// Use this for initialization
+	void Start () {
+		m.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
