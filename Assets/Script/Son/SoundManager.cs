6cf677d6191823ec2ed0820747e06f0500f86862
Jicidusse <45400320+Jicidusse@users.noreply.github.com>
2018-12-01 02:13:53 +0100

    Son integration

---
+++ Assets/Script/Son/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	//var studioSystem = FMODUnity.RuntimeManager.StudioSystem;
+	//FMOD.Studio.CPU_USAGE cpuUsage;
+	//studioSystem.getCPUUsage(out cpuUsage);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
