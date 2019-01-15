ac95a0756207d4a354de7dc74f7c5b70f85fa061
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-01-15 23:09:01 +0100

    attaque speciale

--- Assets/Script/ConsoleScript.cs
+++ Assets/Script/ConsoleScript.cs
@@ -1,17 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ConsoleScript : MonoBehaviour {
 
+public class ConsoleScript : MonoBehaviour 
+{
 	public Vector3 pos;
+	public int RoomNumber = 1;
+	public Text TextADroite;
 	
-	void Start () {
-		
+	public void ActivateText()
+	{
+		switch (RoomNumber)
+		{
+			case 1 :
+				Room1();
+				break;
+			case 2 :
+				Room2();
+				break;
+			case 3 :
+				Room3();
+				break;
+			case 4 :
+				Room4();
+				break;
+			case 5 :
+				Room5();
+				break;
+			case 6 :
+				Room6();
+				break;
+		}
+	}
+
+	public void Room1()
+	{
+		var textToWrite = "Attaque Spéciale Room1";
+		TextADroite.text = textToWrite;
 	}
 	
+	public void Room2()
+	{
+		var textToWrite = "Attaque Spéciale Room2";
+		TextADroite.text = textToWrite;
+	}
 	
-	void Update () {
-		
+	public void Room3()
+	{
+		var textToWrite = "Attaque Spéciale Room3";
+		TextADroite.text = textToWrite;
+	}
+	
+	public void Room4()
+	{
+		var textToWrite = "Attaque Spéciale Room4";
+		TextADroite.text = textToWrite;
+	}
+	
+	public void Room5()
+	{
+		var textToWrite = "Attaque Spéciale Room5";
+		TextADroite.text = textToWrite;
+	}
+	
+	public void Room6()
+	{
+		var textToWrite = "Attaque Spéciale Room6";
+		TextADroite.text = textToWrite;
 	}
 }
--- Assets/Script/PersoScript.cs
+++ Assets/Script/PersoScript.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
-public class PersoScript : MonoBehaviour {
+public class PersoScript : MonoBehaviour 
+{
 
 	NavMeshAgent nma;
-	public bool vaJouerUneCarte = false;
-	public int carteID = -1;
-	CartesManager cartesManager;
 	
-	void Start () {
+	public bool vaJouerUneCarte = false;
+	
+	public int carteID = -1;
+	
+	CartesManager cartesManager;
+	public ConsoleScript WantedConsoleScript;
+	
+	[SerializeField] private GameObject specialAction;
+	
+	void Start () 
+	{
 		nma = GetComponent<NavMeshAgent>();
 		cartesManager = GameObject.Find("GameMaster").GetComponent<CartesManager>();
-	}
+	}	
 	
-	
-	void Update () {
+	void Update () 
+	{
 		if (vaJouerUneCarte)
 		{
 			JouerUneCarte(carteID);
-		}
-		
+		}	
 	}
 
 	public void JouerUneCarte(int cardID)
@@ -32,6 +40,32 @@ public class PersoScript : MonoBehaviour {
 			cartesManager.PlayACardOnModule(carteID);
 			carteID = -1;
 			vaJouerUneCarte = false;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.parent.name == "Console")
+		{
+			var room = other.transform.parent;
+			WantedConsoleScript = room.GetComponent<ConsoleScript>();
+			if (!specialAction.activeInHierarchy)
+			{
+				specialAction.SetActive(true);
+			}
+			WantedConsoleScript.ActivateText();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.transform.parent.name == "Console")
+		{
+			WantedConsoleScript = null;
+			if (specialAction.activeInHierarchy)
+			{
+				specialAction.SetActive(false);
+			}
 		}
 	}
 }
