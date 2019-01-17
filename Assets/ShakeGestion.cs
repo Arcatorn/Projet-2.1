9d17ba821491d76572549846aba68c0a56d8419c
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2019-01-17 07:57:11 +0100

    Added ScreenShake et UI

---
+++ Assets/ShakeGestion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeGestion : MonoBehaviour {
+
+    public Camera MainCamera;
+    public CameraShake CamScript;
+	// Use this for initialization
+	void Start ()
+    {
+    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKey(KeyCode.D))
+        {
+            CamScript.shakeDuration = 10;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+
+        }
+    }
+}
