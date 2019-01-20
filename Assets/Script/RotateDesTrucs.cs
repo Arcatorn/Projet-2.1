2e99d1468220b33760d9d3a13360ccf73f7d10d6
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-01-20 16:51:12 +0100

    scene modif

--- Assets/Script/RotateDesTrucs.cs
+++ Assets/Script/RotateDesTrucs.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateDesTrucs : MonoBehaviour {
+public class RotateDesTrucs : MonoBehaviour
+{
+    public int wantedAxe = 1;
     public float RotateSpeed;
 
-	void Update ()
+    void Update()
     {
-        transform.Rotate(0, RotateSpeed, 0);
+        if (wantedAxe == 0)
+        {
+            transform.Rotate(RotateSpeed, 0, 0);
+        }
+        else if (wantedAxe == 1)
+        {
+            transform.Rotate(0, RotateSpeed, 0);
+        }
+        else
+        {
+            transform.Rotate(0, 0, RotateSpeed);
+        }
     }
 }
