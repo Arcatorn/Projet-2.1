957ff9560d49e791d16aafb8dacd97f52ff2a024
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2019-01-20 22:34:42 +0100

    oui

--- Assets/Script/VehiculeFB.cs
+++ Assets/Script/VehiculeFB.cs
@@ -2,69 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehiculeFB : MonoBehaviour {
+public class VehiculeFB : MonoBehaviour
+{
 
-public static int vehiculeState = 0;
-public GameObject[] particuleFenetreGO;
-public CameraShake cameraShake;
-public GameObject[] tempestGO;
-public GameObject[] particuleMoteur;
-	
-	private void Start() {
-		EtatSurregime();
-	}
+    public static int vehiculeState = 0;
+    public GameObject[] particuleFenetreGO;
+    public CameraShake cameraShake;
+    public GameObject[] tempestGO;
+    public GameObject[] particuleMoteur;
 
-				// ETAT STOP
-				// • Stop ambiance déplacement
+	AmbiantSounds ambiantSounds;
 
-				// ETAT DEPLACEMENT
-				// • Play son d'ambiance
-
-				// ETAT SURREGIME
-				// • Accéléré speed particule devant fenêtres
-				// • Play son surrégime
-				// • Augmente camera shake
-				// • Anim gros moteur Killian
-
-				// ETAT TEMPETE
-				// • Activer gameObject Tempest
-				// • Coordonner son eclair avec anim eclair
-				// • Son tempest ambiance
-				// • Stop particules devant fenêtres
-				// • Randomisation anim eclairs
-
-	public void EtatStop()
+	void Awake()
 	{
-		for (int i = 0; i < particuleFenetreGO.Length; i++)
-		{
-			particuleFenetreGO[i].GetComponent<ParticleSystem>().Pause();
-		}
-		cameraShake.Shake(0,0,0);
+		ambiantSounds = GetComponent<AmbiantSounds>();
 	}
 
-	public void EtatDeplacement()
-	{
-		for (int i = 0; i < particuleFenetreGO.Length; i++)
-		{
-			particuleFenetreGO[i].GetComponent<ParticleSystem>().Play();
-		}
-		cameraShake.Shake(0.05f, 99999, 1);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            EtatStop();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            EtatDeplacement();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            EtatSurregime();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            EtatTempete();
+        }
+		 if (Input.GetKeyDown(KeyCode.M))
+        {
+            StopTempete();
+        }
+    }
 
-	}
+    // ETAT SURREGIME
+    // • Play son surrégime
 
-	public void EtatSurregime()
-	{
-		for (int i = 0; i < particuleFenetreGO.Length; i++)
-		{
-			var main = particuleFenetreGO[i].GetComponent<ParticleSystem>().main;
-			var maBITE = main.startSpeed.constant;
-			maBITE = 5;
-		}
-		cameraShake.Shake(0.15f, 9999, 1);
-	}
+    // ETAT TEMPETE
+    // • Coordonner son eclair avec anim eclair
+    // • Randomisation anim eclairs
 
-	public void EtatTempete()
-	{
+    public void EtatStop()
+    {
+        ambiantSounds.StopAll();
+        for (int i = 0; i < particuleFenetreGO.Length; i++)
+        {
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Pause();
+        }
+        foreach (GameObject go in particuleMoteur)
+        {
+            go.SetActive(false);
+        }
+        cameraShake.Shake(0, 0, 0);
+        particuleMoteur[0].transform.parent.GetComponent<Animator>().SetBool("playanim",false);
+    }
 
-	}
+    public void EtatDeplacement()
+    {
+        ambiantSounds.StopAll();
+        ambiantSounds.PlayNormalAmbiant();
+        for (int i = 0; i < particuleFenetreGO.Length; i++)
+        {
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Stop();
+
+            var main = particuleFenetreGO[i].GetComponent<ParticleSystem>().main;
+            main.startSpeed = new ParticleSystem.MinMaxCurve(2.5f, 5);
+
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Play();
+        }
+        cameraShake.Shake(0.05f, 99999, 1);
+        foreach (GameObject go in particuleMoteur)
+        {
+            go.SetActive(false);
+        }
+        particuleMoteur[0].transform.parent.GetComponent<Animator>().SetBool("playanim",false);
+
+    }
+
+    public void EtatSurregime()
+    {
+        for (int i = 0; i < particuleFenetreGO.Length; i++)
+        {
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Stop();
+
+            var main = particuleFenetreGO[i].GetComponent<ParticleSystem>().main;
+            main.startSpeed = new ParticleSystem.MinMaxCurve(5, 10);
+
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Play();
+        }
+
+        foreach (GameObject go in particuleMoteur)
+        {
+            go.SetActive(true);
+        }
+        particuleMoteur[0].transform.parent.GetComponent<Animator>().SetBool("playanim",true);
+        cameraShake.Shake(0.15f, 9999, 1);
+    }
+
+    public void EtatTempete()
+    {
+        ambiantSounds.StopAll();
+        ambiantSounds.PlayTempeteAmbiant();
+        for (int i = 0; i < tempestGO.Length; i++)
+        {
+            tempestGO[i].SetActive(true);
+            particuleFenetreGO[i].GetComponent<ParticleSystem>().Stop();
+        }
+        foreach (GameObject go in particuleMoteur)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    public void StopTempete()
+    {
+        ambiantSounds.StopAll();
+        for (int i = 0; i < tempestGO.Length; i++)
+        {
+            tempestGO[i].SetActive(false);
+        }
+    }
 }
